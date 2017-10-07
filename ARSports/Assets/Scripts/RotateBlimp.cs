6dd4803d5b0668c1cf6d5169ef55b9c1a0f53205
Roman Jaquez <romanjaquez@dhcp-18-111-18-157.dyn.mit.edu>
2017-10-07 11:03:34 -0400

    placed initial assets on the scene (football field asssets) and created more materials

---
+++ ARSports/Assets/Scripts/RotateBlimp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBlimp : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        transform.Rotate(0, Time.deltaTime * -10, 0);
+    }
+}
