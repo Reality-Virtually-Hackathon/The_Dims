d6418a5642c8f8c6735aaded9c843001973c8a9b
Roman Jaquez <romanjaquez@dhcp-18-111-18-157.dyn.mit.edu>
2017-10-08 15:13:09 -0400

    added screenshots and readme file

---
+++ ARSports/Assets/Scripts/SpatialUnderstandingState.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using UnityEngine;
+using HoloToolkit.Unity;
+//using HoloToolkit.Unity.SpatialMapping;
+
+public class SpatialUnderstandingState : Singleton<SpatialUnderstandingState>
+{
+    public float MinAreaForStats = 5.0f;
+
+    public TextMesh DebugDisplay;
+    //public TextMesh DebugSubDisplay;
+
+    private bool _triggered;
+    public bool HideText = false;
+
+    private string _spaceQueryDescription;
+
+    public string SpaceQueryDescription
+    {
+        get
+        {
+            return _spaceQueryDescription;
+        }
+        set
+        {
+            _spaceQueryDescription = value;
+        }
+    }
+
+    public string PrimaryText
+    {
+        get
+        {
+            if (HideText)
+                return string.Empty;
+
+            // Display the space and object query results (has priority)
+            if (!string.IsNullOrEmpty(SpaceQueryDescription))
+            {
+                return SpaceQueryDescription;
+            }
+
+            // Scan state
+            if (SpatialUnderstanding.Instance.AllowSpatialUnderstanding)
+            {
+                switch (SpatialUnderstanding.Instance.ScanState)
+                {
+                    case SpatialUnderstanding.ScanStates.Scanning:
+                        // Get the scan stats
+                        IntPtr statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
+                        if (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(statsPtr) == 0)
+                        {
+                            return "playspace stats query failed";
+                        }
+
+                        return "Walk around and scan in your playspace";
+                    case SpatialUnderstanding.ScanStates.Finishing:
+                        return "Finalizing scan (please wait)";
+                    case SpatialUnderstanding.ScanStates.Done:
+                        return "Scan complete";
+                    default:
+                        return "ScanState = " + SpatialUnderstanding.Instance.ScanState;
+                }
+            }
+            return string.Empty;
+        }
+    }
+
+    public Color PrimaryColor
+    {
+        get
+        {
+            return Color.white;
+        }
+    }
+
+    public string DetailsText
+    {
+        get
+        {
+            if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.None)
+            {
+                return "";
+            }
+
+            // Scanning stats get second priority
+            if ((SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Scanning) &&
+                (SpatialUnderstanding.Instance.AllowSpatialUnderstanding))
+            {
+                IntPtr statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
+                if (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(statsPtr) == 0)
+                {
+                    return "Playspace stats query failed";
+                }
+                SpatialUnderstandingDll.Imports.PlayspaceStats stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+
+                // Start showing the stats when they are no longer zero
+                if (stats.TotalSurfaceArea > MinAreaForStats)
+                {
+                    SpatialMappingManager.Instance.DrawVisualMeshes = false;
+                    string subDisplayText = string.Format("totalArea={0:0.0}, horiz={1:0.0}, wall={2:0.0}", stats.TotalSurfaceArea, stats.HorizSurfaceArea, stats.WallSurfaceArea);
+                    subDisplayText += string.Format("\nnumFloorCells={0}, numCeilingCells={1}, numPlatformCells={2}", stats.NumFloor, stats.NumCeiling, stats.NumPlatform);
+                    subDisplayText += string.Format("\npaintMode={0}, seenCells={1}, notSeen={2}", stats.CellCount_IsPaintMode, stats.CellCount_IsSeenQualtiy_Seen + stats.CellCount_IsSeenQualtiy_Good, stats.CellCount_IsSeenQualtiy_None);
+                    return subDisplayText;
+                }
+                return "";
+            }
+            return "";
+        }
+    }
+
+    private void Update_DebugDisplay()
+    {
+        // Basic checks
+        if (DebugDisplay == null)
+        {
+            return;
+        }
+
+        // Update display text
+        DebugDisplay.text = PrimaryText;
+        DebugDisplay.color = PrimaryColor;
+        //DebugSubDisplay.text = DetailsText;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Updates
+        Update_DebugDisplay();
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# ARSports
+
+![alt text](Screenshots/startup_screen.png "ARSports Startup Screen")
+
+## Inspiration
+
+The fact that the sports-watching experience is still constrained to just watching it on TV - what if the user can be also made part of the game and experience it in a more immersive way, where he is interacting with the game and making decisions on how to view it, get information from it - not just limited to what the game broadcast offers, but beyond.
+
+Imagine if the Game can be bought to the user as opposed to th user going to the game, and leverage voice-based commands and gestures to interact with the game. This has a potential to apply to any sport with the user being the center of the experience.
+
+This application can be further be enhanced to have a shared viewing experience with friends and can also be supported by VR Devices.
+
+
+ARSports is AR expereince built with Open Source Applications and Tools.
+
+## Assets
+
+Built on Unity 3D - 2017 1.1f1 Personal
+Assets built using Blender 2.78 
+Device support - Micrsoft Hololens
+
+## What it does
+
+Allows the user to have a more immersive experience when it comes to watching any type of sport.
+
+## How to view the application
+
+Clone the Github source code into local machine.
+git clone https://github.com/Reality-Virtually-Hackathon/The_Dims.git
+
+Open the source code in Unity 3D and Generate a build for Hololens and deploy the build on the device.
+
+## Screenshots
+
+![alt text](Screenshots/ARSports_3D.jpg "ARSports experience")
+
+![alt text](Screenshots/ARSports_scrore.jpg "ARSports - Scoreboard view")
+
+![alt text](Screenshots/ARSports_field.jpg "ARSports - Player position view 1")
+
+![alt text](Screenshots/ARSports_fieldView.jpg "ARSports - Player position view 2")
+
+![alt text](Screenshots/ARSports_blip.jpg "ARSports - Ads on blip")
+
+![alt text](Screenshots/ARSports_Experince.jpg "ARSports - Ordering food")
+
