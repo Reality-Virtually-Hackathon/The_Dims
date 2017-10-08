4fd1ed157db79808f0847d10763839fa3a2c18ae
Roman Jaquez <romanjaquez@dhcp-18-111-18-157.dyn.mit.edu>
2017-10-08 11:11:56 -0400

    added football ball, trayectory and tooltip

---
+++ ARSports/Assets/Scripts/FootballScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootballScript : MonoBehaviour
+{
+    public GameObject footballTooltip;
+
+    // Use this for initialization
+    void Start()
+    {
+        footballTooltip.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGazeEnter()
+    {
+        // OnRowSelection();
+    }
+
+    void OnGazeExit()
+    {
+        // GameLogic.Instance.ResetRows();
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        footballTooltip.SetActive(footballTooltip.activeSelf ? false : true);
+    }
+
+    void OnMouseDown()
+    {
+        footballTooltip.SetActive(footballTooltip.activeSelf ? false : true);
+    }
+}
